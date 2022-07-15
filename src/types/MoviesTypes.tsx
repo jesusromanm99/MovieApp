cf0ba5afddeee315daabdaf289e100be3130fd6d
jesus <jesusromanm99@fpuna.edu.py>
2022-07-14 21:46:32 -0400

    Movie type interface

---
+++ src/types/MoviesTypes.tsx
@@ -0,0 +1,10 @@
+export type Movie = {
+  Title: string;
+  Poster: string;
+  imdbRating: string;
+  Plot?: string | null;
+  Actors?: string | null;
+  Director?: string | null;
+  imdbID: string;
+  Genre?: string;
+};
